e04c71d1a43cce1c69f04813a326462fa820a9a2
chouti <chouti@gmail.com>
2011-02-25 08:02:52 +0800

    add track event

--- js/a.js
+++ js/a.js
@@ -2,6 +2,10 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-57044-26']);
 _gaq.push(['_trackPageview']);
 
+function trackLi(li_id) {
+    _gaq.push(['_trackEvent', 'li' + li_id, 'clicked']);
+  };
+
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
