d4838ea3e3bde268d349e10f802c574a1b7aef74
chouti <chouti@gmail.com>
2011-03-21 15:54:39 +0800

    add #task_list div

--- js/didit2.js
+++ js/didit2.js
@@ -1,8 +1,12 @@
 function FirstSelect(){
 	if(localStorage.user_token==undefined){
-		$('#nav_about').removeClass('not_select').addClass('select')
+		$('#nav_about').removeClass('not_select').addClass('select');
+		$('#review').show();
+		$('#task_list').hide();
 	} else {
 		$('#nav_home').removeClass('not_select').addClass('select')
+		$('#review').hide();
+		$('#task_list').show();
 	}
 }
 
